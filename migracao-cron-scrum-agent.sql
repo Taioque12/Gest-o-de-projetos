f9ef4494891aacb4a65f18330adb5c823b26f596
Leonardo Taioque Machado <leonardo.machado@maconeglian.com.br>
2026-07-17 09:24:29 -0300

    chore: atualiza SQL do cron com service_role_key real

--- migracao-cron-scrum-agent.sql
+++ migracao-cron-scrum-agent.sql
@@ -7,16 +7,13 @@ CREATE EXTENSION IF NOT EXISTS pg_net;
 CREATE EXTENSION IF NOT EXISTS pg_cron;
 
 -- 3. Agendar a chamada HTTP para a Edge Function todo dia às 03:00 da manhã
--- Substitua a URL base pela URL real do seu projeto. 
--- Obs: Como estamos no SQL Editor remoto, nós já saberemos a URL (https://uaooutzbxkkcyfuwijbi.supabase.co)
-
 SELECT cron.schedule(
     'scrum-master-diario',
-    '0 3 * * *', -- Roda todos os dias às 03:00 am
+    '0 3 * * *',
     $$
     SELECT net.http_post(
         url:='https://uaooutzbxkkcyfuwijbi.supabase.co/functions/v1/scrum-agent',
-        headers:=jsonb_build_object('Content-Type', 'application/json', 'Authorization', 'Bearer SEU_SERVICE_ROLE_KEY')
+        headers:=jsonb_build_object('Content-Type', 'application/json', 'Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhb291dHpieGtrY3lmdXdpamJpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MjA5NTYyNCwiZXhwIjoyMDk3NjcxNjI0fQ.5S3dtP-6dQXYxV--aw0oxw1ehltg7rJEVGZJ3Di6d6k')
     );
     $$
 );
